062170ace384ff3464860fae5d0e755f89b491bc
Core Alcoser <corrinaalcoser@gmail.com>
2025-05-26 12:49:19 -0500

    included render into the App function

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 ## Text-RPG README
 This is README for Text-based RPG game.
 
-npm run build
---
+++ text-rpg/text-rpg/src/App.js
@@ -0,0 +1,13 @@
+import TextRPG from './TextRPG';
+
+function App() {
+  render () {
+    return (
+      <div className="App">
+        <TextRPG />
+      </div>
+    );
+  }
+}
+
+export default App;
